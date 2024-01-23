9d12c63ed755bb6dc3aa5b7cff522b7531b0029c
user <hag.hispin12@gmail.com>
2024-01-23 18:39:04 +0200

    version5

--- app/src/main/java/com/tom/mathproject_tom_m/MainActivity.java
+++ app/src/main/java/com/tom/mathproject_tom_m/MainActivity.java
@@ -3,13 +3,24 @@ package com.tom.mathproject_tom_m;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button etgar;
+    private Button ad20;
+    private Button loach ;
+    private TextView firstrandom;
+    private TextView secondrandom;
+    private EditText enteranswer;
+    private Button  check;
+    private Button  save;
+    private Button  allpar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int n =13;
+
     }
 }
