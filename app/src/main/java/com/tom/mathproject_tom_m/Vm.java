f795a3b9b329a04de7a3ed01ec2b964c28af8a73
user <hag.hispin12@gmail.com>
2024-02-07 21:01:39 +0200

    version8 i build Vm

---
+++ app/src/main/java/com/tom/mathproject_tom_m/Vm.java
@@ -0,0 +1,13 @@
+package com.tom.mathproject_tom_m;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class Vm extends ViewModel {
+    MutableLiveData<Integer> Vnum1;
+    MutableLiveData<Integer>Vnum2;
+    Exersise exersise;
+
+
+
+}
