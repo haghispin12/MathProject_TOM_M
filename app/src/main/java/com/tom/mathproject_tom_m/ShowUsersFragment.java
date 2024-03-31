373edd2b7f222c557661ff98fcba432bcb281d0e
user <hag.hispin12@gmail.com>
2024-03-31 17:41:56 +0300

    version9 i finish matala 6

--- app/src/main/java/com/tom/mathproject_tom_m/ShowUsersFragment.java
+++ app/src/main/java/com/tom/mathproject_tom_m/ShowUsersFragment.java
@@ -1,14 +1,28 @@
 package com.tom.mathproject_tom_m;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import kotlin.Result;
 
 
 public class ShowUsersFragment extends Fragment {
@@ -16,6 +30,29 @@ public class ShowUsersFragment extends Fragment {
 Vm MainViewmodel;
 TextView user1;
     TextView rate23;
+    Button picture;
+    Uri uri;
+    ImageView pic1;
+
+    ActivityResultLauncher<Intent> startCamera = registerForActivityResult(
+
+            new ActivityResultContracts.StartActivityForResult(),
+
+            new ActivityResultCallback<ActivityResult>() {
+
+                @Override
+
+                public void onActivityResult(ActivityResult result) {
+
+                    if (result.getResultCode() == RESULT_OK) {
+
+                      pic1.setImageURI(uri);
+
+                    }
+
+                }
+
+            });
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,5 +78,27 @@ TextView user1;
         user1.setText(MainViewmodel.getUsername());
       rate23=view.findViewById(R.id.rate23);
        rate23.setText(MainViewmodel.getUserRate()+"");
+        pic1=view.findViewById(R.id.pic1);
+        picture=view.findViewById(R.id.picture);
+        picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+
+                values.put(MediaStore.Images.Media.TITLE, "New Picture");
+
+                values.put(MediaStore.Images.Media.DESCRIPTION, "From Camera");
+
+                uri =
+                        requireContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+
+                startCamera.launch(cameraIntent);
+            }
+        });
     }
+
 }
