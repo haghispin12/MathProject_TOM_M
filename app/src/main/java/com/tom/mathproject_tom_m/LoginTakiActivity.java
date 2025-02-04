a8d159567a63b2699655fd67cb61f62cefc68ef2
user <hag.hispin12@gmail.com>
2025-02-04 15:15:24 +0200

    Start project taki

--- app/src/main/java/com/tom/mathproject_tom_m/LoginTakiActivity.java
+++ app/src/main/java/com/tom/mathproject_tom_m/LoginTakiActivity.java
@@ -3,6 +3,7 @@ package com.tom.mathproject_tom_m;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,17 +11,35 @@ import android.widget.TextView;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginTakiActivity extends AppCompatActivity {
-Button btlogin;
+
 Button btConnected;
 TextView tvNameOfGame;
 EditText etEmail;
 EditText etPassowrd;
-Button btSubmit;
+Button btSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_taki2);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         int n=10;
+        initview();
+    }
+
+    private void initview() {
+        btSignUp=findViewById(R.id.btSignUp);
+        btConnected=findViewById(R.id.btConnected);
+        btConnected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
