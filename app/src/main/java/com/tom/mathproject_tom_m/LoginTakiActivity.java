ef8bebdf73bfc164cebec11e10151caefbc0b5e0
user <hag.hispin12@gmail.com>
2025-02-19 12:31:32 +0200

    הרשמה וכניסה עם פיירבייס

--- app/src/main/java/com/tom/mathproject_tom_m/LoginTakiActivity.java
+++ app/src/main/java/com/tom/mathproject_tom_m/LoginTakiActivity.java
@@ -1,28 +1,39 @@
 package com.tom.mathproject_tom_m;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.tom.mathproject_tom_m.mathproj.LoginActivity;
+import com.tom.mathproject_tom_m.mathproj.MainActivity;
 
 public class LoginTakiActivity extends AppCompatActivity {
 
 Button btConnected;
 TextView tvNameOfGame;
 EditText etEmail;
+    private FirebaseAuth auth ;
 EditText etPassowrd;
 Button btSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_taki2);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        int n=10;
+       // int n=10;
         initview();
     }
 
@@ -32,12 +43,39 @@ Button btSignUp;
         btConnected.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                auth.signInWithEmailAndPassword(etEmail.getText().toString(),etPassowrd.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
 
+                            Toast.makeText(LoginTakiActivity.this, "Authentication success.", Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                            Toast.makeText(LoginTakiActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                Intent intent = new Intent(LoginTakiActivity.this, MainActivityTaki123.class);
+                startActivity(intent);
             }
         });
         btSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+               auth= FirebaseAuth.getInstance();
+                if(auth.getCurrentUser()!=null){
+                    auth.createUserWithEmailAndPassword(etEmail.getText().toString(),etPassowrd.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                                Toast.makeText(LoginTakiActivity.this,"Registration success",Toast.LENGTH_SHORT).show();
+                             else{
+                                Toast.makeText(LoginTakiActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
 
             }
         });
